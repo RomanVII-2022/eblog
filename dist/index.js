249882a99318addb982fdde6fe44466ce58504bc
RomanVII-2022 <vicmkinyua77@gmail.com>
2024-05-08 11:30:48 +0300

    sequalize dotenv joi

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -8,6 +17,26 @@ const models_1 = __importDefault(require("./models"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.get('/', (request, response) => __awaiter(void 0, void 0, void 0, function* () {
+    const users = yield models_1.default.Blogger.findAll();
+    return response.status(200).json({
+        success: true,
+        data: users
+    });
+}));
+app.post('/', (request, response) => __awaiter(void 0, void 0, void 0, function* () {
+    const newUser = {
+        name: request.body.name,
+        email: request.body.email,
+        password: request.body.password
+    };
+    const user = yield models_1.default.Blogger.create(newUser);
+    return response.status(201).json({
+        success: true,
+        data: user
+    });
+}));
 models_1.default.sequelize.authenticate().then(() => {
     app.listen(process.env.SERVER_PORT, () => {
         console.log("Server is running on port " + process.env.SERVER_PORT);
--- index.ts
+++ index.ts
@@ -1,9 +1,32 @@
 import express from "express";
+import { Request, Response } from "express-serve-static-core";
 import db from "./models";
 import dotenv from "dotenv";
 dotenv.config()
 
 const app = express()
+app.use(express.json())
+
+app.get('/', async (request: Request, response: Response) => {
+    const users = await db.Blogger.findAll()
+    return response.status(200).json({
+        success: true,
+        data: users
+    })
+})
+
+app.post('/', async (request: Request, response: Response) => {
+    const newUser = {
+        name: request.body.name,
+        email: request.body.email,
+        password: request.body.password
+    }
+    const user = await db.Blogger.create(newUser)
+    return response.status(201).json({
+        success: true,
+        data: user
+    })
+})
 
 db.sequelize.authenticate().then(() => {
     app.listen(process.env.SERVER_PORT, () => {
